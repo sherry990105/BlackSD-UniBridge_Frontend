3b5a7d0d70133159a6cb80a2852442dfe25dd12b
kimjh371441 <kimjh371441@gmail.com>
2026-03-09 14:00:22 +0900

    260309 14:00 마이페이지와 헤더 연결중

---
+++ frontend/assets/js/user/mentor/myPage/userMatching/userMatching.js
@@ -0,0 +1,34 @@
+const modal = document.getElementById("matchingModal");
+const openBtn = document.getElementById("openModalBtn");
+const xBtn = document.querySelector(".closeBtn");    // 상단 X 버튼
+const closeBtn = document.getElementById("closeModalBtn");
+const submitBtn = document.getElementById("sumbitBtn"); //작성완료
+
+// 모달 열기
+openBtn.onclick = () => {
+    modal.style.display = "block";
+}
+
+// 모달 닫기 (취소 버튼 또는 X 버튼)
+[closeBtn, xBtn].forEach(btn => {
+    btn.onclick = () => {
+        modal.style.display = "none";
+    }
+});
+
+// 모달 바깥쪽 어두운 배경 클릭 시 닫기
+window.onclick = (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+submitBtn.onclick = (event) => {
+    // 1. 폼 전송으로 인한 새로고침 방지
+    event.preventDefault();
+
+    // 2. 원하는 경로를 직접 지정하여 이동합니다.
+    // 예: "userModify.html" 또는 "../../main.html" 등
+    const targetPath = "../../../../../../html/user/mentee/myPage/myPage.html"; 
+    window.location.href = targetPath;
+};
